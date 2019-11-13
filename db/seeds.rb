f67dba37d686c91c80c501f62263a28341bc3de6
Matthew Biela <matt.biela@gmail.com>
2019-11-13 22:36:43 +0100

    basic yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,20 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cate = ["chinese", "italian", "japanese", "french", "belgian"]
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Food.sushi,
+    address: Faker::Address.street_address,
+    category: cate.sample
+  )
+  restaurant.save!
+end
+
+puts 'Finished!'
